4cef2d77254e2d0491235be330b7e0f8343cad9d
Priyesh <91priyesh@gmail.com>
2019-03-05 12:49:23 -0500

    Updates

---
+++ precache-manifest.16826584ca677bf0a6e2f6ef4bb6092c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "a62195643a9efcebc166",
+    "url": "/clicky-game/static/js/main.a6219564.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/clicky-game/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "d74f62b762f1f2c3543a1d79b1281816",
+    "url": "/clicky-game/index.html"
+  }
+];
